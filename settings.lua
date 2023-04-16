5c0bbfa07d9d97c25fa78ac3b78ef1fc194e4d56
Aleks247 <aleks247@abv.bg>
2023-04-16 03:34:07 +0300

    https://github.com/aleks247/tli-spawnselector

---
+++ settings.lua
@@ -0,0 +1,41 @@
+Settings = {}
+
+Settings.Locations = {
+---Ems
+    { name = "hospital",
+    label = "Pillbox Hospital",
+    job = "ambulance",
+    coords = vector3(270.32, -564.83, 53.52),
+    rot = vector3(-29.46, -0.0, -123.25),
+    spawn = vector4(299.64801, -575.1514, 43.260845, 76.685317) },
+---Police
+    { name = "lspd",
+    job = "police", 
+    label = "Los Santos Police Department",
+    coords = vector3(401.63, -978.86, 45.244),
+    rot = vector3(-26.97, -0.0, -105.56),
+    spawn = vector4(428.26644, -984.8966, 30.712158, 1.9879124) }, 
+
+    { name = "sspd",
+    job = "police" ,
+    label = "Sandy Police Department",
+    coords = vector3(1854.272, 3662.382, 38.67),
+    rot = vector3(-13.88, 2.1344, -13.59),
+    spawn = vector4(1853.7364, 3680.9487, 34.26662, 209.94386) },
+    
+    { name = "ppd",
+    job = "police" ,
+    label = "PaletoBay Police Department",
+    coords = vector3(-400.59, 6016.4, 39.85),
+    rot = vector3(-14.85577, -0.0, 101.91),
+    spawn = vector4(-446.12, 6024.19, 31.49, 319.81) },
+
+
+---Civilians
+    { name = "cityhall",
+    label = "City Hall",
+    coords = vector3(-532.52, -237.43, 49.22),
+    rot = vector3(-23.58, -0.0, 17.22),
+    spawn = vector4(-536.2222, -226.3347, 37.666244, 212.51023) },
+
+}
